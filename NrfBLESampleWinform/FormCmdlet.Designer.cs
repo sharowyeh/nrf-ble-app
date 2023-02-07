fe3eacd6edeaa39ddc882351ae6a61fcee34e81b
sharow yeh <yuyu.yei@hotmail.com>
2023-02-07 18:31:14 +0800

    add cmdlet form to write BLE data in winform sample project because im lazy

---
+++ NrfBLESampleWinform/FormCmdlet.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace NrfBLESampleWinform
+{
+    partial class FormCmdlet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewCmdlets = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCmdlets)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewCmdlets
+            // 
+            this.dataGridViewCmdlets.AllowUserToAddRows = false;
+            this.dataGridViewCmdlets.AllowUserToDeleteRows = false;
+            this.dataGridViewCmdlets.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewCmdlets.ColumnHeadersVisible = false;
+            this.dataGridViewCmdlets.Location = new System.Drawing.Point(13, 13);
+            this.dataGridViewCmdlets.Name = "dataGridViewCmdlets";
+            this.dataGridViewCmdlets.ReadOnly = true;
+            this.dataGridViewCmdlets.RowHeadersVisible = false;
+            this.dataGridViewCmdlets.Size = new System.Drawing.Size(309, 336);
+            this.dataGridViewCmdlets.TabIndex = 0;
+            // 
+            // FormCmdlet
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(334, 361);
+            this.Controls.Add(this.dataGridViewCmdlets);
+            this.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormCmdlet";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "FormCmdlet";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCmdlets)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewCmdlets;
+    }
+}
---
+++ NrfBLESampleWinform/FormCmdlet.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NrfBLESampleWinform
+{
+    public partial class FormCmdlet : Form
+    {
+        private string cmdletFile = "";
+        private BindingSource source = new BindingSource();
+        private List<Cmdlet> cmdlets = new List<Cmdlet>();
+
+        public FormCmdlet(string filepath)
+        {
+            InitializeComponent();
+
+            LoadCmdletFile(filepath);
+
+            source.DataSource = cmdlets;
+            dataGridViewCmdlets.DataSource = source;
+            dataGridViewCmdlets.Columns.Insert(0, new DataGridViewButtonColumn { Text = "Run", UseColumnTextForButtonValue = true});
+            dataGridViewCmdlets.CellClick += DataGridViewCmdlets_CellClick;
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            // modify to hide instead of close from x button
+            if (e.CloseReason != CloseReason.None &&
+                e.CloseReason != CloseReason.UserClosing)
+                return;
+            this.Hide();
+            e.Cancel = true;
+        }
+
+        public void LoadCmdletFile(string filepath)
+        {
+            if (string.IsNullOrWhiteSpace(filepath) || System.IO.File.Exists(filepath) == false)
+                return;
+
+            cmdlets.Clear();
+            cmdletFile = filepath;
+            using (var sr = new System.IO.StreamReader(filepath))
+            {
+                var line = sr.ReadLine();
+                while (line != null)
+                {
+                    line = line.Trim();
+                    if (line.Length > 0 && line.Trim().StartsWith("#") == false)
+                    {
+                        var cmdlet = Cmdlet.ParseCmdlet(line);
+                        if (cmdlet != null)
+                            cmdlets.Add(cmdlet);
+                    }
+                    line = sr.ReadLine();
+                }
+            }
+
+            source.DataSource = cmdlets;
+            source.ResetBindings(false);
+        }
+
+        private void DataGridViewCmdlets_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // only works for the button column
+            if (e.ColumnIndex == 0)
+            {
+                var cmdlet = cmdlets[e.RowIndex];
+                Form1.SetCmdlet(cmdlet.Direction, cmdlet.ReportChar, cmdlet.HexString);
+            }
+        }
+    }
+
+    public class Cmdlet
+    {
+        public string Caption { get; set; }
+        
+        public string ReportChar { get; set; }
+        
+        // READ or WRITE
+        public string Direction { get; set; }
+        
+        // 2bytes hex string with whitespace separator
+        public string HexString { get; set; }
+        
+        public Cmdlet(string caption, string reportChar, string direction, string hexString)
+        {
+            Caption = caption;
+            ReportChar = reportChar;
+            Direction = direction;
+            HexString = hexString;
+        }
+
+        public static Cmdlet ParseCmdlet(string cmdletString)
+        {
+            var sep = cmdletString.Trim().Split(',');
+            if (sep.Length < 4)
+            {
+                return null;   
+            }
+            var cmdlet = new Cmdlet(sep[0].Trim(), sep[1].Trim(), sep[2].Trim(), sep[3].Trim());
+            //TODO: do more data validation here?
+            return cmdlet;
+        }
+    }
+}
